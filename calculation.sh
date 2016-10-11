4fef6ce894fe85065684a093c28bec21cc42a7a2
root <capwone@gmail.com>
2016-10-11 17:01:37 -0400

    fix unit.

--- calculation.sh
+++ calculation.sh
@@ -12,5 +12,5 @@ for i in `seq 0 3`; do
 	cat vm$i.dat | grep "Request" | cut -d " " -f  7 >> "temp.dat"
 done
 
- cat temp.dat | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq /     NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print "number of requests" ", "sum / NR ", "     sqrt(sqd)}' >> result.csv
+ cat temp.dat | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq /     NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print "number of requests per sec" ", "sum / NR ", "     sqrt(sqd)}' >> result.csv
 
