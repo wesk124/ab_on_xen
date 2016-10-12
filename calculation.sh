1ef906f152a7c868a886653b8a9bd35e05e99f20
root <capwone@gmail.com>
2016-10-12 04:08:31 -0400

    fix calculaiton.sh

--- calculation.sh
+++ calculation.sh
@@ -14,3 +14,5 @@ done
 
  cat temp.dat | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq /     NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print "number of requests per sec" ", "sum / NR ", "     sqrt(sqd)}' >> result.csv
 
+echo "please check the result in result.csv"
+
