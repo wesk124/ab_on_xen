c17fa3e206deac964adc5ccd6a942dac817f9e1d
root <capwone@gmail.com>
2016-10-28 14:59:51 -0400

    add data

--- calculation.sh
+++ calculation.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 source pv-deploy-config.mk
 
 POSTFIX=".dat"
 TEMP="temp"
 
-echo -n "" > temp.dat;
-echo -n "" > result.csv
-for i in `seq $ID $((VM_NUM-1))`; do
-	cat vm$i.dat | grep "Request" | cut -d " " -f  7 >> "temp.dat"
+echo -n "" > temp$1.dat;
+echo -n "" > result$1.csv
+for i in `seq $ID $(($1-1))`; do
+	cat vm$i.dat | grep "Request" | cut -d " " -f  7 >> "temp$1.dat"
 done
 
- cat temp.dat | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq /     NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print "number of requests per sec" ", "sum / NR ", "     sqrt(sqd)}' >> result.csv
+ cat temp$1.dat | awk  '{sum += $1; sumsq += ($1 * $1) } END {sqd = sumsq /     NR - (sum / NR)^2; if(sqd < 10^-8) sqd = 0; print  "sum / NR ", "     sqrt(sqd)}' >> result$1.csv
+
+
+mkdir data$1 
+
+mv *.dat *.csv ./data$1
 
 echo "please check the result in result.csv"
 
--- start.sh
+++ start.sh
@@ -6,22 +6,27 @@ source "pv-deploy-config.mk";
 IP_BASE=`echo $GATEWAY | cut -d"." -f1-3`.
 
 function run_apache {
+    printf "please enter how many server you want for this run:\n";
+    read numOfServers;
+    if [ $numOfServers -gt $VM_NUM ]; then
+        echo "number of servers cannot be greater than number of vm"
+        exit
+    fi
 
     printf "function test\n";
-    printf "Running $VM_NUM server(s) benchmark will start in 3 secs...\n"
-#    mkdir "data"$VM_NUM
+    printf "Running $numOfServers server(s) benchmark will start in 3 secs...\n"
 
-    for i in `seq 0 $((VM_NUM-1))`; do 
+
+    for i in `seq 0 $((numOfServers-1))`; do 
         touch $PREFIX$i.dat;
-        ab -n 10000 -c 50  "http://"$IP_BASE$(($BASE_IP_NUM+$i))"/" >> $PREFIX$i.dat &
+        ab -n 10000 -c 50  "http://"$IP_BASE$(($BASE_IP_NUM+$i))"/" >> $PREFIX$i.dat  &
     done
     wait
 }
 
 echo "start experiment..."
-rm -r data* || true     # remove previous data
 rm result*.csv || true  # remove previous calculation
-rm vm*.dat
+rm vm*.dat  ||true
 wait
 echo "old data cleaned"
 
